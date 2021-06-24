e689618f0b0fb0daf7fc8a8949ca9a0645cea83f
Jack Curtin <jackcurtin94@gmail.com>
2021-06-24 13:25:38 -0500

    added cart placeholder for css styling of nav

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -5,11 +5,22 @@ export default function Navbar (){
         <>
             <nav className="navbar">
                 <div className="navbar-container">
+                    <ul className="nav-menu">
+                        <li className="nav-item">
+                            <p className="nav-link">Stores</p>
+                        </li>
+                        <li className="nav-item">
+                            <p className="nav-link">Contact</p>
+                        </li>
+                    </ul>
                     <Link href="/">
                         <h1 className="navbar-logo">
                             FOXTROT
                         </h1>
                     </Link>
+                    <p className="nav-cart-icon">
+                        test
+                    </p>
                 </div>
             </nav>
         </>
