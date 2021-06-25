5e47a24283b2131558817c04b91068c6d15f958a
Jack Curtin <jackcurtin94@gmail.com>
2021-06-25 08:35:06 -0500

    menu bars added

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,12 +1,34 @@
 import Link from "next/link";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import {faBars, faShoppingCart, faTimes} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useEffect, useState} from "react";
 
 export default function Navbar (){
+    const [click, setClick] = useState(false);
+    const [menuBars, setBars] = useState(true);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showBars = () => {
+        if(window.innerWidth <= 960) {
+            setBars(false);
+        } else{
+            setBars(true);
+        }
+    }
+
+    useEffect(() => {
+        showBars();
+        window.addEventListener('resize', showBars)
+    }, []);
+
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
+                    <div className="menu-bars-icon" onClick={handleClick}>
+                        <FontAwesomeIcon icon={click ? faTimes : faBars} />
+                    </div>
                     <ul className="nav-menu">
                         <li className="nav-item">
                             <p className="nav-link">Stores</p>
