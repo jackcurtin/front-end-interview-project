7209bce2660d10b2358a6837427cde52082acad5
Jack Curtin <jackcurtin94@gmail.com>
2021-07-02 10:11:44 -0500

    added comments

--- components/products/ProductGroup.js
+++ components/products/ProductGroup.js
@@ -4,15 +4,17 @@ export default function ProductGroup(props){
         <>
             <h2>{props.title}</h2>
             <ul className={"product-grid"}>
-                {/*within each product group, generates a card for each product over the span of the group's grid*/}
+                {/* within each product group, generates a card for each product over the span of the group's grid */}
                 {products.map(product => (
                     <div className="product-card">
+                        {/* All products only have one asset in the array - the thumbnail */}
                         <img src={product.assets[0].url}
                         className="product-image"/>
                         <p className="product-title">
                             {product.title}
                         </p>
                         <p className="product-price">
+                            {/* creates new formatting for USD currency, then applies to unit price */}
                             {new Intl.NumberFormat("en-US",
                                 {style: "currency",
                                 currency: "USD"}).format(product.unitPrice)}
